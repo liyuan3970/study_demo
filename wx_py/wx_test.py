879043afaa16223c0527bd0dba31853d69aa18e0
liyuan3970 <857956255@qq.com>
2019-07-27 22:44:28 +0800

    wx_bot

--- wx_py/wx_test.py
+++ wx_py/wx_test.py
@@ -12,5 +12,47 @@
 2.用time模块定义发送天气预报的时间，并利用wxpy推送消息
 
 '''
+
+#msg = '您好我是李渊的微信机器人'
+
+
+# 返回所有好友的列表
+# a.bot.friends()
+
+# 向所有好友发送消息
+# a.send('str')
+
+#自动响应各类请求
+######################################################
+
+# 打印来自其他好友、群聊和公众号的消息
+#@bot.register()
+#def print_others(msg):
+#    print(msg)
+
+
+
+# 自动接受新的好友请求
+#@bot.register(msg_types=FRIENDS)
+#def auto_accept_friends(msg):
+#    # 接受好友请求
+#    new_friend = msg.card.accept()
+#    # 向新的好友发送消息
+#    new_friend.send('哈哈，我自动接受了你的好友请求')
+
+
+##############################################################
 from wxpy import *
-bot = Bot()
+bot=Bot(cache_path=True)
+
+
+@bot.register() # 接收从指定好友发来的消息，发送者即recv_msg.sender为指定好友girl_friend
+def recv_send_msg(recv_msg):
+    print('收到的消息：',recv_msg.text) # recv_msg.text取得文本
+    return '今夜台州气温15度'
+
+#embed()
+
+bot.join()
+
+
