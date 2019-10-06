9e78fd674aaae823f37f0776b3c82a9d7630830f
liyuan3970 <857956255@qq.com>
2019-10-06 17:58:02 +0800

    django

--- Django/blogdemo/index/admin.py
+++ Django/blogdemo/index/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    #显示所有文章的字段
+    list_display = ('post_name', 'time', 'auther','tag')
+    # 对应的字段可以作为超链接进去
+    list_display_links = ('auther', 'tag')
+    # 自爱文章列表中可以修改的字段，和link不能重叠
+    list_editable = ('time',)
+
+
+    # 在文章列表右侧添加一个过滤的选项
+    list_filter = ('auther',)
+    # 在文章列表顶部添加一个搜索狂，方便用户查找，里面的字段是查询的关键字
+    search_fields = ('post_name', 'time')
+
+    # 这个编辑页面的基本布局
+    fieldsets = (
+        #组1-基本选项,包含post_name和auther两个列
+        ('基本选项',{
+            'fields':('post_name','auther')
+        }),
+        #组2-高级选项,包含tag和time和content
+        ('高级选项',{
+            'fields':('tag','time','content'),
+            'classes':('content',)
+        })
+    )
+
+
+
+
+
+admin.site.register(Auther)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag)
--- Django/blogdemo/index/models.py
+++ Django/blogdemo/index/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50, null=False, verbose_name="名称")
+
+    class Meta:
+        verbose_name = verbose_name_plural = '类别'
+
+    def __str__(self):
+        return self.tag_name
+
+
+class Auther(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '男性'),
+        (STATUS_DELETE, '女性'),
+    )
+    # null = True的时候才是设置不能为空！
+    auther_name = models.CharField(max_length=50, null=True, verbose_name="作者姓名")
+
+    sex = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="作者性别")
+
+    age = models.IntegerField(verbose_name="作者年龄")
+
+    class Meta:
+        verbose_name = verbose_name_plural = '作者'
+
+    # 这个是在admin中返回作者的创建成功后的列表
+    def __str__(self):
+        return self.auther_name
+
+
+class Post(models.Model):
+    post_name = models.CharField(max_length=50, null=True, verbose_name="名称")
+    time = models.TimeField()
+    auther = models.ForeignKey(Auther, verbose_name="作者姓名", on_delete=models.DO_NOTHING)
+    tag = models.ForeignKey(Tag, verbose_name="文章类别", on_delete=models.DO_NOTHING)
+    content = models.TextField(max_length=50, null=True, verbose_name="文章内容")
+
+    class Meta:
+        verbose_name = verbose_name_plural = '文章'
+
+    def __str__(self):
+        return self.post_name
