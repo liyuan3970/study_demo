8c66985dab6176a49178b829819c7ef673ffd751
liyuan3970 <857956255@qq.com>
2019-10-09 15:38:32 +0800

    templates和view的简单配置

--- Django/blogdemo/index/views.py
+++ Django/blogdemo/index/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def index(request):
-    return HttpResponse("传递进来的参数值为:")
+    tag = Tag.objects.all()
+    post = Post.objects.raw('select * from index_post')
+    auther = Auther.objects.raw('select id,auther_name from index_auther')
+    return render(request,'index.html',locals())
