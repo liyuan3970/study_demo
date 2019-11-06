a5bb9a5051c9849d8f97bd5e135620cc0ecbb586
liyuan3970 <857956255@qq.com>
2019-11-06 15:16:39 +0800

    ip

--- Django/blogdemo/blogdemo/settings.py
+++ Django/blogdemo/blogdemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lx=o1lvakyx^6vtn=$w21x)d=g(#s2gw#jtcxehkoya4wqu39d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','127.0.0.1']
 
 
 # Application definition
