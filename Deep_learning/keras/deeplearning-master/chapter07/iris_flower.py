0d741cf85cdedce410004bbe38fa6c898f68e611
liyuan3970 <857956255@qq.com>
2020-04-08 11:14:12 +0800

    Keras

--- Deep_learning/keras/deeplearning-master/chapter07/iris_flower.py
+++ Deep_learning/keras/deeplearning-master/chapter07/iris_flower.py
@@ -5,6 +5,7 @@ from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from tensorflow.python.client import device_lib
 
 # 导入数据
 dataset = datasets.load_iris()
