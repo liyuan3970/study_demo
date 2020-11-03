c890cf653b5c6276359e73e0deb23c730aa6ff18
liyuan3970 <857956255@qq.com>
2020-11-03 23:21:15 +0800

    data

--- Bootstrap/Tz_demo/pie.js
+++ Bootstrap/Tz_demo/pie.js
@@ -1,91 +1,134 @@
+var weatherIcons = {
+    'Sunny': '1.gif',
+    'Cloudy': '2.gif',
+    'Showers': '3.gif'
+};
+
 option = {
+    title: {
+        text: '天气情况统计',
+        subtext: '虚构数据',
+        left: 'center'
+    },
     tooltip: {
         trigger: 'item',
-        formatter: '{a} <br/>{b}: {c} ({d}%)'
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
     },
     legend: {
-        orient: 'vertical',
-        left: 10,
-        data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
+        // orient: 'vertical',
+        // top: 'middle',
+        bottom: 10,
+        left: 'center',
+        data: ['西凉', '益州', '兖州', '荆州', '幽州']
     },
     series: [
         {
-            name: '访问来源',
             type: 'pie',
+            radius: '65%',
+            center: ['50%', '50%'],
             selectedMode: 'single',
-            radius: [0, '30%'],
-
-            label: {
-                position: 'inner'
-            },
-            labelLine: {
-                show: false
-            },
             data: [
-                {value: 335, name: '直达', selected: true},
-                {value: 679, name: '营销广告'},
-                {value: 1548, name: '搜索引擎'}
-            ]
-        },
-        {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['40%', '55%'],
-            label: {
-                formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
-                backgroundColor: '#eee',
-                borderColor: '#aaa',
-                borderWidth: 1,
-                borderRadius: 4,
-                // shadowBlur:3,
-                // shadowOffsetX: 2,
-                // shadowOffsetY: 2,
-                // shadowColor: '#999',
-                // padding: [0, 7],
-                rich: {
-                    a: {
-                        color: '#999',
-                        lineHeight: 22,
-                        align: 'center'
-                    },
-                    // abg: {
-                    //     backgroundColor: '#333',
-                    //     width: '100%',
-                    //     align: 'right',
-                    //     height: 22,
-                    //     borderRadius: [4, 4, 0, 0]
-                    // },
-                    hr: {
-                        borderColor: '#aaa',
-                        width: '100%',
-                        borderWidth: 0.5,
-                        height: 0
-                    },
-                    b: {
-                        fontSize: 16,
-                        lineHeight: 33
-                    },
-                    per: {
-                        color: '#eee',
-                        backgroundColor: '#334455',
-                        padding: [2, 4],
-                        borderRadius: 2
+                {
+                    value: 1548,
+                    name: '幽州',
+                    label: {
+                        formatter: [
+                            '{title|{b}}{abg|}',
+                            '  {weatherHead|天气}{valueHead|天数}{rateHead|占比}',
+                            '{hr|}',
+                            '  {Sunny|}{value|202}{rate|55.3%}',
+                            '  {Cloudy|}{value|142}{rate|38.9%}',
+                            '  {Showers|}{value|21}{rate|5.8%}'
+                        ].join('\n'),
+                        backgroundColor: '#eee',
+                        borderColor: '#777',
+                        borderWidth: 1,
+                        borderRadius: 4,
+                        rich: {
+                            title: {
+                                color: '#eee',
+                                align: 'center'
+                            },
+                            abg: {
+                                backgroundColor: '#333',
+                                width: '100%',
+                                align: 'right',
+                                height: 25,
+                                borderRadius: [4, 4, 0, 0]
+                            },
+                            Sunny: {
+                                height: 30,
+                                align: 'left',
+                                backgroundColor: {
+                                    image: weatherIcons.Sunny
+                                }
+                            },
+                            Cloudy: {
+                                height: 30,
+                                align: 'left',
+                                backgroundColor: {
+                                    image: weatherIcons.Cloudy
+                                }
+                            },
+                            Showers: {
+                                height: 30,
+                                align: 'left',
+                                backgroundColor: {
+                                    image: weatherIcons.Showers
+                                }
+                            },
+                            weatherHead: {
+                                color: '#333',
+                                height: 24,
+                                align: 'left'
+                            },
+                            hr: {
+                                borderColor: '#777',
+                                width: '100%',
+                                borderWidth: 0.5,
+                                height: 0
+                            },
+                            value: {
+                                width: 20,
+                                padding: [0, 20, 0, 30],
+                                align: 'left'
+                            },
+                            valueHead: {
+                                color: '#333',
+                                width: 20,
+                                padding: [0, 20, 0, 30],
+                                align: 'center'
+                            },
+                            rate: {
+                                width: 40,
+                                align: 'right',
+                                padding: [0, 10, 0, 0]
+                            },
+                            rateHead: {
+                                color: '#333',
+                                width: 40,
+                                align: 'center',
+                                padding: [0, 10, 0, 0]
+                            }
+                        }
                     }
+                },
+                {value: 535, name: '荆州'},
+                {value: 510, name: '兖州'},
+                {value: 634, name: '益州'},
+                {value: 735, name: '西凉'}
+            ],
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
                 }
-            },
-            data: [
-                {value: 335, name: '直达'},
-                {value: 310, name: '邮件营销'},
-                {value: 234, name: '联盟广告'},
-                {value: 135, name: '视频广告'},
-                {value: 1048, name: '百度'},
-                {value: 251, name: '谷歌'},
-                {value: 147, name: '必应'},
-                {value: 102, name: '其他'}
-            ]
+            }
         }
     ]
 };
+
 
 var pie = echarts.init(document.getElementById('pie'),'dark');
 pie.setOption(option);
