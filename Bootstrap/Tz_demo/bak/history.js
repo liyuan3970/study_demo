e5a3fa78006bcf3050ec409a380c398f2159cb82
liyuan3970 <857956255@qq.com>
2020-11-05 21:44:07 +0800

    data

--- Bootstrap/Tz_demo/bak/history.js
+++ Bootstrap/Tz_demo/bak/history.js
@@ -18,17 +18,10 @@ option = {
         bottom: '3%',
         containLabel: true
     },
-    xAxis: [{
+    xAxis: {
         type: 'value',
-        offset: 10,
-        position: 'top',
-        boundaryGap: [0, 0.09]
-    },
-    {
-        type: 'value',
-        position: 'bottom',
         boundaryGap: [0, 0.01]
-    }],
+    },
     yAxis: {
         type: 'category',
         data: ['第一', '第二', '第三', '第四', '第五', '第六']
@@ -37,13 +30,12 @@ option = {
         {
             name: '降水',
             type: 'bar',
-            xAxisIndex: 1,
-            data: [55, 60, 170, 210, 350, 550]
+            data: [18203, 23489, 29034, 104970, 131744, 630230]
         },
         {
-            name: '气温',
+            name: '2012年',
             type: 'bar',
-            data: [3, 13, 25, 30, 38, 44]
+            data: [19325, 23438, 31000, 121594, 134141, 681807]
         }
     ]
 };
