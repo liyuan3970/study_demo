36b896a512d237b203c9a7c578882c3127803a8b
“liyuan3970” <857956255@qq.com>
2020-11-15 19:24:16 +0800

    data

--- Django/Tz_demo/index/comparebar.py
+++ Django/Tz_demo/index/comparebar.py
@@ -50,9 +50,34 @@ def read_sql_station(station):
     return averge
 
 
+def return_sql_station():
+    print("this is a read station file")
+    server = "172.21.158.201"    # 连接服务器地址
+    user = "down"# 连接帐号
+    password = "downx"# 连接密码
+    conn = pymssql.connect(server, user, password, "ZJSZDZDB")  #获取连接
+    cursor = conn.cursor() # 获取光标
+    cursor.execute('USE ZJSZDZDB' )
+    sql = "select IIiii ,avg(fFy),avg(T),avg(RR) FROM TAB_Aws WHERE \
+          (IIiii ='58559' or IIiii ='58652' or IIiii ='58568' or IIiii = '58660'  \
+          or IIiii ='58665' or IIiii = '58664' or IIiii = '58667' or IIiii ='58665')  \
+          AND (tTime BETWEEN " + "'" + \
+          str(first_day_of_last_month) + "'" + "AND" + "'" + str(last_day_of_last_month) + "'" + ") group BY IIiii"
+    cursor.execute(sql)
+    row = cursor.fetchall()
+    data = pd.DataFrame(list(row))
+    sql2 = "select IIiii ,avg(fFy),avg(T ),avg(RR) FROM Tab_MWS WHERE \
+          (IIiii ='K8201' or IIiii ='K8302' or IIiii ='K8505')  \
+          AND (tTime BETWEEN " + "'" + \
+          str(first_day_of_last_month) + "'" + "AND" + "'" + str(last_day_of_last_month) + "'" + ") group BY IIiii"
+    cursor.execute(sql2)
+    row2 = cursor.fetchall()
+    data2 = pd.DataFrame(list(row2))
 
 
+    print(data2)
 
+return_sql_station()
 
 
 
